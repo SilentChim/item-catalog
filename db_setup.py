bb5c2137c1af6e4b06b7249a070f62cd9f91c6cc
Peyton Lucas <plucas@Nomad.local>
2017-07-31 10:20:39 -0500

    Add mapper variables to methods

--- db_setup.py
+++ db_setup.py
@@ -14,23 +14,41 @@ class User(Base):
     # Defines the name of the table
     __tablename__ = 'user'
     # Mapper variables for user attributes
+    name = Column(String(80), nullable = False)
+    email = Column(String(80), nullable = False)
+    picture = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
 
-class Feature(Base):
+class Application(Base):
     """Class defines the table for the features in the database"""
     # Defines the name of the table
-    __tablename__ = 'feature'
+    __tablename__ = 'application'
+    # Defines relationships with other tables
     user = relationship(User)
     # Mapper variables for feature attributes
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    name = Column(String(250), nullable = False)
 
-class featureItem(Base):
+class Feature(Base):
     """Class defines the table for the items in the database"""
     # Defines the name of the table
-    __tablename__ = 'feature_item'
-    feature = relationship(Feature)
+    __tablename__ = 'feature'
+    # Defines relationships with other tables
+    application = relationship(Application)
     user = relationship(User)
     # Mapper variables for feature item attributes
+    id = Column(Integer, primary_key=True)
+    title = Column(String(80), nullable = False)
+    description = Column(String(250))
+    client = Column(String(80), nullable = False)
+    client_priority = Column(Integer)
+    target_date = Column(String(6), nullable = False)
+    product_area = Column(String(80), nullable = False)
+    application_id = Column(Integer,ForeignKey('application.id'))
 
 
 # End configuration code
-engine = create_engine('sqlite:///featureitem.db')
+engine = create_engine('sqlite:///application.db')
 Base.metadata.create_all(engine)
