ef7aa9da7751f3208e4c3d0470d1c67f7d6dbae7
Peyton Lucas <plucas@Nomad.local>
2017-07-31 14:41:57 -0500

    Add methods and routes

--- application.py
+++ application.py
@@ -5,13 +5,32 @@ from database_setup import Base, Restaurant, MenuItem
 
 app = Flask(__name__)
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///application.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+# Shows all applications in the database
+@app.route('/')
+@app.route('/application')
+def showApplications():
+    return "This page will display all created applications"
 
+# Creates a new application by the user
+@app.route('/application/new')
+def newApplication():
+    return "This page will create a new application for the user"
+
+# Edits application created by the user
+@app.route('/application/<int:application_id>/edit')
+def editApplication():
+    return "This page will edit an application created by the user"
+
+# Deletes application created by the user
+@app.route('/application/<int:application_id>/delete')
+def deleteApplication():
+    return "This page will delete an application created by the user"
 
 if __name__ == '__main__':
     app.debug = True
