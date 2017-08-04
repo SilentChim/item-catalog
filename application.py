1bf8b7369aebdc547acc49def587194d7dd7f915
Peyton Lucas <plucas@Nomad.local>
2017-08-04 12:36:09 -0500

    Add JSON API endpoints

--- application.py
+++ application.py
@@ -11,6 +11,26 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+# JSON APIs to view Application Info
+@app.route('/application/<int:application_id>/feature/JSON')
+def applicationFeatureJSON(application_id):
+    application = session.query(Application).filter_by(id=application_id).one()
+    features = session.query(Feature).filter_by(
+        application_id=application_id).all()
+    return jsonify(Features=[f.serialize for f in features])
+
+
+@app.route('/application/<int:application_id>/feature/<int:feature_id>/JSON')
+def featureJSON(application_id, feature_id):
+    App_Feature = session.query(Feature).filter_by(id=feature_id).one()
+    return jsonify(App_Feature=App_Feature.serialize)
+
+
+@app.route('/restaurant/JSON')
+def applicationsJSON():
+    applications = session.query(Application).all()
+    return jsonify(applications=[a.serialize for a in applications])
+
 # Main Handlers
 # Shows all applications
 @app.route('/')
@@ -92,10 +112,10 @@ def editFeature(application_id, feature_id):
             editedFeature.description = request.form['description']
         if request.form['client']:
             editedFeature.client = request.form['client']
-        if request.form['client_priority']:
-            editedFeature.client_priority = request.form['client_priority']
-        if request.form['target_date']:
-            editedFeature.target_date = request.form['target_date']
+        # if request.form['client_priority']:
+        #     editedFeature.client_priority = request.form['client_priority']
+        # if request.form['target_date']:
+        #     editedFeature.target_date = request.form['target_date']
         if request.form['product_area']:
             editedFeature.product_area = request.form['product_area']
         session.add(editedFeature)
